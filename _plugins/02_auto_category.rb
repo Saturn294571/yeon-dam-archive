2f8cb692606976bb43642c218450f62c436a8c49
Saturn294571 <97665295+Saturn294571@users.noreply.github.com>
2025-09-22 01:24:34 +0900

    Update 02_auto_category.rb
    Signed-off-by: Saturn294571 <97665295+Saturn294571@users.noreply.github.com>

--- _plugins/02_auto_category.rb
+++ _plugins/02_auto_category.rb
@@ -1,14 +1,23 @@
 # _plugins/02_auto_category.rb
 
 Jekyll::Hooks.register :posts, :pre_render do |post|
-  dir_path = File.dirname(post.path)
-  relative_dir = dir_path.sub(post.site.source, '')
-
-  # 경로를 폴더 이름들로 분할합니다.
-  # 맨 앞의 '/' 와 최상위 폴더인 'contents'를 제거합니다.
-  categories = relative_dir.split('/').reject { |c| c.empty? || c == 'contents' }
-
+  # --- 해결책 2: 카테고리 경로 수정 ---
+  # 머리말에 카테고리가 수동으로 지정되지 않은 경우에만 실행
   if post.data['categories'].nil? || post.data['categories'].empty?
-    post.data['categories'] = categories
+    dir_path = File.dirname(post.path)
+    relative_dir = dir_path.sub(post.site.source, '')
+    
+    path_parts = relative_dir.split('/').reject(&:empty?)
+    
+    # 'contents' 폴더의 위치를 찾음
+    contents_index = path_parts.index('contents')
+    
+    if contents_index
+      # 'contents' 폴더 바로 다음부터 끝까지를 카테고리로 설정
+      # 예: [Private_arcive, 1_Project, 4-1 학부, ...]
+      categories = path_parts.slice(contents_index + 1, path_parts.length)
+      post.data['categories'] = categories
+    end
   end
+  # ------------------------------------
 end
